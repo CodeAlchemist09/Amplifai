98838bb957dca432fd3bfae9b7f27c690b848014
CodeAlchemist09 <dipankargalaxy1020@gmail.com>
2026-08-16 14:30:52 +0530

    Update ScrollToTop.tsx

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,13 +1,1 @@
-"use client";
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
 
-export function ScrollToTop() {
-  const { pathname } = usePathname();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
-}
